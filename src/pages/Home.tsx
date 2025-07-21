87b9005491f7703b6f2c44287e6236d614500119
Ovett Mora <ovettmora17@gmail.com>
2025-07-20 22:23:48 -0600

    add Home and NotFound pages with routing setup in App component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,21 @@
+import Navbar from "../components/Navbar";
+import StarBackground from "../components/StarBackground";
+import ThemeToggle from "../components/ThemeToggle";
+
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Background Effects */}
+      <StarBackground />
+      {/* Navbar */}
+      <Navbar />
+      {/* Main Content */}
+
+      {/* Footer */}
+    </div>
+  );
+}
+
+export default Home;
