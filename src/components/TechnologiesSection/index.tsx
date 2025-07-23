34ae127fa5ca8e38a553c58a7b008884c8bb903d
Ovett Mora <ovettmora17@gmail.com>
2025-07-23 02:03:31 -0600

    refactor: update layout and styling in GitHubStats and TechnologiesIntro components

--- src/components/TechnologiesSection/index.tsx
+++ src/components/TechnologiesSection/index.tsx
@@ -6,11 +6,11 @@ const TechnologiesSection = () => {
   return (
     <section
       id="technologies"
-      className="bg-background-secondary min-h-screen py-24 px-4 relative"
+      className="bg-background-secondary py-24 px-4 relative"
     >
       <div className="container max-w-5xl flex flex-col items-center">
         <TechnologiesHeader />
-        <div className="flex flex-col justify-center items-center flex-grow gap-15">
+        <div className="flex flex-col justify-center items-center gap-10 w-full">
           <TechnologiesIntro />
           <TechnologiesCarousel />
         </div>
