df6bca865daba29a2ef5e53a88686be615828824
Ovett Mora <ovettmora17@gmail.com>
2025-07-24 22:57:29 -0600

    feat: enhance Project interface with additional fields for slug, role, and extended descriptions

--- src/types/Project.ts
+++ src/types/Project.ts
@@ -1,8 +1,12 @@
 export interface Project {
   id: number;
   title: string;
+  slug: string;
+  role: string;
   description: string;
+  descriptionExtend?: string;
   image: string;
+  videoDemo?: string;
   tags: string[];
   demoUrl?: string;
   codeUrl?: string;
