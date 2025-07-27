4cacd05dbb4c49197c1e685caedec23128d31fca
Ovett Mora <ovettmora17@gmail.com>
2025-07-26 23:44:20 -0600

    feat: implement internationalization for About section components and replace static data with dynamic translations

--- src/components/AboutSection/AboutCards.tsx
+++ src/components/AboutSection/AboutCards.tsx
@@ -1,12 +1,18 @@
-import { aboutMeCards } from "@/data/aboutMeCards";
+import { useTranslation } from "react-i18next";
+import { getAboutMeCards } from "@/data/getAboutMeCards";
 import AboutCard from "./AboutCard";
 
-const AboutCards = () => (
-  <div className="grid grid-cols-1 gap-6">
-    {aboutMeCards.map((card, i) => (
-      <AboutCard key={i} {...card} />
-    ))}
-  </div>
-);
+const AboutCards = () => {
+  const { t } = useTranslation("about");
+  const aboutMeCards = getAboutMeCards(t);
+
+  return (
+    <div className="grid grid-cols-1 gap-6">
+      {aboutMeCards.map((card, i) => (
+        <AboutCard key={i} {...card} />
+      ))}
+    </div>
+  );
+};
 
 export default AboutCards;
--- src/utils/i18n/i18n.ts
+++ src/utils/i18n/i18n.ts
@@ -5,6 +5,9 @@ import enNavbar from "@/utils/locales/en/navbar/navbar.json";
 import esNavbar from "@/utils/locales/es/navbar/navbar.json";
 import enHero from "@/utils/locales/en/hero/hero.json";
 import esHero from "@/utils/locales/es/hero/hero.json";
+import enAbout from "@/utils/locales/en/about/about.json";
+import esAbout from "@/utils/locales/es/about/about.json";
+
 
 const pathLangMatch = window.location.pathname.match(/^\/(en|es)(\/|$)/);
 const detectedLanguage = pathLangMatch ? pathLangMatch[1] : "en";
@@ -18,10 +21,12 @@ i18n
       en: {
         navbar: enNavbar,
         hero: enHero,
+        about: enAbout,
       },
       es: {
         navbar: esNavbar,
         hero: esHero,
+        about: esAbout,
       },
     },
     lng: savedLanguage, // Usa el idioma de la URL, localStorage o "en" por defecto
